85ecb8bdf262466fee72a4a87015a39dd45bd5a4
Fandy <1223305593@qq.com>
2016-11-09 14:11:25 +0800

    ceshi

--- src/main/java/com/huan/visit/httphelper/Main.java
+++ src/main/java/com/huan/visit/httphelper/Main.java
@@ -3,7 +3,7 @@ package com.huan.visit.httphelper;
 public class Main {
 
 	public static void main(String[] args) {
-		String web1="https://don-fandy.rhcloud.com/";
+		String web1="https://don-fandy.rhcloud.com/beta2/index.jsp";
 		String web2="https://util-fandy.rhcloud.com/input.action";
 		new Thread(new VisitThread(web1)).start();
 		new Thread(new VisitThread(web2)).start();
